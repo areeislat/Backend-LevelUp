89bb6f3e5521f3b2314b1e9e5fa0842c4fa203b5
Marcelo Palma <mar.palmal@duocuc.cl>
2025-12-03 04:44:22 -0300

    Pequeños cambios

--- insert-reward.js
+++ insert-reward.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Reward = require('./src/models/loyalty/Reward'); 
 // Asegúrate de poner TU string de conexión real aquí abajo
-const MONGODB_URI = 'mongodb+srv://marpalmal:awNRTG1Ur8gt3t6H@proyecto.qbt3om8.mongodb.net/LevelUP?retryWrites=true&w=majority';
+const MONGODB_URI = 'mongodb+srv://are_isla:F4vrroh70AOLNnaM@cluster0.2ka54bc.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(MONGODB_URI).then(async () => {
   console.log('Conectado...');
